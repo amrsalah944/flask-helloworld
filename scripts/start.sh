901b7cc8486b6d8769abb9f13d8cbbad774b05bc
Amr Salah <amrsalah@localhost.localdomain>
2019-09-16 20:13:19 +0800

    troubleshooting3

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,4 +2,4 @@ cd /home/ec2-user/myapp
 echo PWD $(pwd)
 pip install --no-cache-dir -r requirements.txt
 echo PWD $(pwd)
-gunicorn -c gunicorn_config.py flask-example:app
+gunicorn -c gunicorn_config.py flask-example:app &
